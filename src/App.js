a1c6196e0e23ad1c2f3ee6aa56c38c4eee8d9ebd
Haitham Alhad Hyder <hyder@adobe.com>
2021-06-11 19:10:56 -0400

    ✨ feat: add control flow in jsx

--- src/App.js
+++ src/App.js
@@ -1,56 +1,15 @@
 import "./App.css";
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  );
+function SecretComponent() {
+  return <h1>Secret information for authorized users only</h1>;
 }
 
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjective} food around</p>
-      <img
-        src="https://github.com/inventrohyder.png"
-        height={200}
-        alt="A human"
-      />
-      <ul style={{ textAlign: "left" }}>
-        {props.dishes.map((dish, i) => (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  );
+function RegularComponent() {
+  return <h1>Everyone can see this component.</h1>;
 }
 
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-}
-
-const dishes = [
-  "Macaroni and Cheese",
-  "Salmon",
-  "Tofu with Vegetables",
-  "Minestrone",
-];
-
-const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Horacio" />
-      <Main adjective="amazing" dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+function App(props) {
+  return <>{props.authorized ? <SecretComponent /> : <RegularComponent />}</>;
 }
 
 export default App;
