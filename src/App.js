fba454e1e7a59a9595f1313ef869e9a34aea1fe2
Haitham Alhad Hyder <hyder@adobe.com>
2021-06-11 18:42:18 -0400

    ✨ feat: use keys to keep track of li

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function Header(props) {
-  console.log(props);
   return (
     <header>
       <h1>{props.name}'s Kitchen</h1>
@@ -15,8 +13,8 @@ function Main(props) {
     <section>
       <p>We serve the most {props.adjective} food around</p>
       <ul style={{ textAlign: "left" }}>
-        {props.dishes.map((dish) => (
-          <li>{dish}</li>
+        {props.dishes.map((dish, i) => (
+          <li key={dish.id}>{dish.title}</li>
         ))}
       </ul>
     </section>
@@ -31,13 +29,20 @@ function Footer(props) {
   );
 }
 
-const dishes = ["Macaroni and Cheese", "Salmon", "Tofu with Vegetables"];
+const dishes = [
+  "Macaroni and Cheese",
+  "Salmon",
+  "Tofu with Vegetables",
+  "Minestrone",
+];
+
+const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
 
 function App() {
   return (
     <div className="App">
       <Header name="Horacio" />
-      <Main adjective="amazing" dishes={dishes} />
+      <Main adjective="amazing" dishes={dishObjects} />
       <Footer year={new Date().getFullYear()} />
     </div>
   );
