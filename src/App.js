172d4e29bf102ce8e73e96c955c810d73fd0edd1
Haitham Alhad Hyder <hyder@adobe.com>
2021-06-11 18:35:29 -0400

    ✨ feat: render list dynamically

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ function Main(props) {
   return (
     <section>
       <p>We serve the most {props.adjective} food around</p>
+      <ul style={{ textAlign: "left" }}>
+        {props.dishes.map((dish) => (
+          <li>{dish}</li>
+        ))}
+      </ul>
     </section>
   );
 }
@@ -26,11 +31,13 @@ function Footer(props) {
   );
 }
 
+const dishes = ["Macaroni and Cheese", "Salmon", "Tofu with Vegetables"];
+
 function App() {
   return (
     <div className="App">
       <Header name="Horacio" />
-      <Main adjective="amazing" />
+      <Main adjective="amazing" dishes={dishes} />
       <Footer year={new Date().getFullYear()} />
     </div>
   );
