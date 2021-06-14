7aa044d38020209a7960171329f5577910104613
Haitham Alhad Hyder <hyder@adobe.com>
2021-06-14 14:20:41 -0400

    ✨ feat: use nested links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Home, About, Events, Contact, Whoops404 } from "./pages";
+import {
+  Home,
+  About,
+  Events,
+  Contact,
+  Whoops404,
+  Services,
+  CompanyHistory,
+  Location,
+} from "./pages";
 
 // https://api.github.com/users/inventrohyder
 
@@ -10,7 +19,11 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />}>
+          <Route path="services" element={<Services />} />
+          <Route path="history" element={<CompanyHistory />} />
+          <Route path="location" element={<Location />} />
+        </Route>
         <Route path="/events" element={<Events />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/*" element={<Whoops404 />} />
